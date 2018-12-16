32ccb0ad081d2ba27744be4f347dbd8f40e07f29
ChiefDakota <17812895+ChiefDakota@users.noreply.github.com>
2018-12-16 08:28:04 -0800

    Clean up Homework #1 solution

--- index_solution.js
+++ index_solution.js
@@ -5,14 +5,10 @@ document.getElementById("heart_icon").addEventListener("click", function() {
 // Homework #1 - Make it so that someone can enter another comment
 document.getElementById("comment_icon").addEventListener("click", function() {
   var user = prompt("What's your name?");
-  if (user != null) {
-    document.getElementById("comment").innerHTML =
-      `<b>${user}</b>`;
-  }
   var comment = prompt("Enter your comment below!");
-  if (comment != null) {
-    var c = document.getElementById("comment");
-    c.insertAdjacentHTML("beforeend", " " + comment)
+  if (user != null && comment != null) {
+    document.getElementById("comment").innerHTML =
+      `<b>${user}</b>` + " " + comment;
   }
 });
 
