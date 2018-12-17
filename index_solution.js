09eedfed43a4f8e6b8d38078f5f3529507d2847a
ChiefDakota <17812895+ChiefDakota@users.noreply.github.com>
2018-12-16 23:18:55 -0800

    Remove unnecessary code

--- index_solution.js
+++ index_solution.js
@@ -6,10 +6,8 @@ document.getElementById("heart_icon").addEventListener("click", function() {
 document.getElementById("comment_icon").addEventListener("click", function() {
   var user = prompt("What's your name?");
   var comment = prompt("Enter your comment below!");
-  if (user != null && comment != null) {
-    document.getElementById("comment").innerHTML =
-      `<b>${user}</b>` + " " + comment;
-  }
+  document.getElementById("comment").innerHTML =
+    `<b>${user}</b>` + " " + comment;
 });
 
 // Homework #2 - Make it so that someone can upload another picture
