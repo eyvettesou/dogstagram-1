a5a54a3c518461d0a03617a867fec629e5f3eab7
ChiefDakota <17812895+ChiefDakota@users.noreply.github.com>
2019-01-13 16:25:08 -0800

    Update images and poster link, and add HTML challenges

---
+++ old/main.js
@@ -0,0 +1,9 @@
+document.querySelector(".heart").addEventListener("click", function() {
+  alert("Liked! ❤️");
+});
+
+// Challenge #J1 - Make it so that someone can post another comment!
+
+// Challenge #J2 - Make it so that when someone clicks on the heart icon the # of likes increases by 1!
+
+// Challenge #J3 - Make it so that someone can upload and post another picture!
